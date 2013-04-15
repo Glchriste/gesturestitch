ed277b2ce015935980097115884f6cf8793a339a
Grace <glchriste@gmail.com>
2013-04-15 16:15:21 -0400

    I made a lot of changes...

--- scripts/leapcanvas.js
+++ scripts/leapcanvas.js
@@ -1,4 +1,5 @@
 var LeapEx = {
+  //Useful variables.
   ws: null,
   ctx: null,
   width: null,
@@ -13,8 +14,10 @@ var LeapEx = {
   leapMaxZ: 180,
   started: false,
 
+  //Initialize the app.
   init: function(el, debugEl) {
 
+    //JQuery Stuff
     LeapEx.el = $(el);
     LeapEx.debugEl = $(debugEl);
 
@@ -24,155 +27,245 @@ var LeapEx = {
       WebSocket = MozWebSocket;
     }
 
+    //Window width and height.
     var w = LeapEx.width = $(window).width();
     var h = LeapEx.height = $(window).height();
     $(el).attr('width', w).css('width', w).attr('height', h).css('height', h);
     $(el).css('position', 'absolute').css('left', '0').css('top', '0');
 
-      var pic = gs.Image.all[1];
-      var im = pic.image.offset();
-      console.log(im);
-      var x1 = im.left;
-      var y1 = im.top;
-      var startX = im.left;
-      var startY = im.top;
-      var imW = gs.Image.all[1].image.width();
-      var imH = gs.Image.all[1].image.height();
-      var x2 = x1 + imW;
-      var y2 = y1 + imH;
-      var xCenter = (x1 + x2)/2;
-      var yCenter = (y1 + y2)/2;
-      var matches = 0;
-      var pic_2 = gs.Image.all[1];
-      var im_2 = pic_2.image.offset();
-      var x1_2 = im.left;
-      var y1_2 = im.top;
-      var startX_2 = im.left;
-      var startY_2 = im.top;
-      var x2_2 = x1 + pic_2.image.width();
-      var y2_2 = y1 + pic_2.image.height();
-      var xCenter_2 = (x1 + x2)/2;
-      var yCenter_2 = (y1 + y2)/2;
-      var imW_2 = gs.Image.all[2].image.width();
-      var imH_2 = gs.Image.all[2].image.height();
+    //Grab a starting image for when Leap interaction starts.
+    var pic = gs.Image.all[1];
+    var im = pic.image.offset();
+    var x1 = im.left;
+    var y1 = im.top;
 
+    //Setup up the 2D context
     LeapEx.ctx = $(el)[0].getContext("2d");
     LeapEx.ws = new WebSocket("ws://localhost:6437/");
 
+    //Setup the Leap functions...
+    
+    //On connection
     LeapEx.ws.onopen = function(event) {
       LeapEx.debug("WebSocket connection open!");
+      LeapEx.ws.send(JSON.stringify({enableGestures: true}));
     };
-
+    //On close
     LeapEx.ws.onclose = function(event) {
       LeapEx.ws = null;
       LeapEx.debug("WebSocket connection closed");
     };
-
+    //On error
     LeapEx.ws.onerror = function(event) {
       LeapEx.debug("Received error");
     };
     
-    var hovering = false;
-    var offset = gs.Image.all[1].image.offset();
-    offset.left = startX;
-    offset.top = startY;
+    //Setup variables.
+    var selection = 1;
+    var selection2 = 2;
+    var previousFrame = null;
+    var gesture = "";
+    var previousSelection = null;
+    var previousSelection2 = null;
+    var matches = 0;
+    
+    //When the Leap receives a new frame, get the data.
     LeapEx.ws.onmessage = function(event) {
+    var selectionCounter = 0;
+    var selectionCounter2 = 0;
 
       if (LeapEx.started) {
         var obj = JSON.parse(event.data);
         var str = JSON.stringify(obj, undefined, 2);
-
+        var frame = obj;
         LeapEx.debug(str);
 
+        //If there are hands, start checking for gestures...
         if (typeof(obj.hands) != 'undefined' && obj.hands.length > 0) {
           var targets = [];
 
-          // for (var i=0; i<obj.hands.length; i++) {
-          //   var hand = obj.hands[i];
-          //   var x = hand.palmPosition[0];
-          //   var y = hand.palmPosition[1];
-          //   var z = hand.palmPosition[2];
-
-          //   if (z < 10) { z = 10; }
-          //   targets.push({ 'x': x, 'y': y, 'z': z });
-          // }
-            var hand = obj.hands[0];
+          //Get palm vectors
+          for (var i=0; i<obj.hands.length; i++) {
+            var hand = obj.hands[i];
             var x = hand.palmPosition[0];
             var y = hand.palmPosition[1];
             var z = hand.palmPosition[2];
 
             if (z < 10) { z = 10; }
             targets.push({ 'x': x, 'y': y, 'z': z });
+          }
 
+          //Get Leap cursor position. 2D screen-projection of 3D ray coming from the center of the palm
           LX = LeapEx.scale(obj.hands[0].palmPosition[0], LeapEx.leapMinX, LeapEx.leapMaxX, -100, LeapEx.width);
           LY = LeapEx.scale(obj.hands[0].palmPosition[1], LeapEx.leapMinY, LeapEx.leapMaxY, LeapEx.height, -100);
           
-          LX = LX - 200;
-          LY = LY - 200;
+          LX = LX - 100;
+          LY = LY - 100;
 
-         //console.log('Pointing Coords: ' + LX + ', ' + LY);
-         gs.Image.all[2].setupCanvas();
-         gs.Image.all[1].setupCanvas();
-         var width = gs.Image.all[2].width;
-         var height = gs.Image.all[2].height;
-         if(matches == 0) { LeapEx.draw(targets); }
-         //If one finger, move left half of image
-         if(obj.pointables.length == 1 && obj.hands.length == 1) {
-          
-          if(matches == 0) { hovering = true; }
-            if(LX >= x1 && LX <= (x1+width) && LY >= y1 && LY <= (y2+height)) {
-              console.log('INTERSECTION');
+         //Draw new cursor.
+         LeapEx.draw(targets);
+         
+         //Setup images.
+         if(selection2 !== null) {
+          gs.Image.all[selection2].setupCanvas();
+         }
+         if(selection !== null) {
+          gs.Image.all[selection].setupCanvas();
+         }
+
+         //Get height and width of images.
+         var width = gs.Image.all[selection].width;
+         var height = gs.Image.all[selection].height;
+
+          // Display Gesture object data if needed. For testing purposes.
+          var gestureString = "";
+          if (frame.gestures.length > 0) {
+            for (var i = 0; i < frame.gestures.length; i++) {
+              var gesture = frame.gestures[i];
+              gestureString += "Gesture ID: " + gesture.id + ", "
+                            + "type: " + gesture.type + ", "
+                            + "state: " + gesture.state + ", "
+                            + "hand IDs: " + gesture.handIds.join(", ") + ", "
+                            + "pointable IDs: " + gesture.pointableIds.join(", ") + ", "
+                            + "duration: " + gesture.duration + " &micro;s, ";
+
+              switch (gesture.type) {
+                case "circle":
+                  gesture = "circle";
+                  gestureString += "center: " + (gesture.center) + " mm, "
+                                + "normal: " + (gesture.normal, 2) + ", "
+                                + "radius: " + gesture.radius + " mm, "
+                                + "progress: " + gesture.progress + " rotations";
+                  break;
+                case "swipe":
+                  gesture = "swipe";
+                  gestureString += "start position: " + (gesture.startPosition) + " mm, "
+                                + "current position: " + (gesture.position) + " mm, "
+                                + "direction: " + (gesture.direction, 2) + ", "
+                                + "speed: " + gesture.speed + " mm/s";
+                  break;
+                case "screenTap":
+                case "keyTap":
+                  gesture = "Tap";
+                  gestureString += "position: " + (gesture.position) + " mm, "
+                                + "direction: " + (gesture.direction, 2);
+                  break;
+                default:
+                  gestureString += "unkown gesture type";
+              }
+            }
+          }
+
+         //Get data from the last frame to compare to the current frame to see if a gesture happened.
+         if(previousFrame != null && frame.gestures.length == 1) {
+           if(previousFrame.gestures.length == 0) {
+             console.log("Gesture happened: " + gesture);
+             switch(gesture) {
+              case "circle":
+                console.log("Changing selection");
+                
+                //Scenarios to switch image selection. When a swipe or circle gesture occurs.     
+                if(frame.hands.length == 1) {
+                  previousSelection = selection;
+                  if(selectionCounter >= 2) {
+                  gs.Image.all[previousSelection].wrapper.removeClass("ui-selected");
+                  gs.Image.all[previousSelection].parent.deselect(gs.Image.all[previousSelection]);
+                  selectionCounter--;
+                }
+                  
+                  if(selection < 6) {
+                    selection++;
+                  } else {
+                    selection = 0;
+                  }
+                if(!gs.Image.all[selection].wrapper.hasClass("ui-selected")) {
+                      gs.Image.all[selection].wrapper.addClass("ui-selected");
+                      gs.Image.all[selection].parent.select(gs.Image.all[selection]);
+                      selectionCounter++;
+              }
+                } else {
+                  previousSelection2 = selection2;
+                  if(selectionCounter2 >= 2) {
+                  gs.Image.all[previousSelection2].wrapper.removeClass("ui-selected");
+                  gs.Image.all[previousSelection2].parent.deselect(gs.Image.all[previousSelection2]);
+                  selectionCounter2--;
+                }
+                  
+                  if(selection2 < 6) {
+                    selection2 = Math.floor(Math.random()*8);
+                    while(selection2 == selection) {
+                      selection2 = Math.floor(Math.random()*8);
+                    }
+                  } else {
+                    selection2 = 0;
+                  }
+                  if(!gs.Image.all[selection2].wrapper.hasClass("ui-selected")) {
+                    gs.Image.all[selection2].wrapper.addClass("ui-selected");
+                    gs.Image.all[selection2].parent.select(gs.Image.all[selection2]);
+                    selectionCounter2++;
+                  }
+                }
+                console.log('Selected image #' + selection);
+                break;
+             }
            }
-            gs.Image.all[1].place(LX, LY);
-            locX = Math.abs(LX);
-            locY = Math.abs(LY);
-            offset.left = locX;
-            offset.top = locY;
-            gs.Image.all[1].image.offset = offset;
-            //console.log(gs.Image.all[1].image.offset.left + ', ' + offset.top);
-            if(!gs.Image.all[1].wrapper.hasClass("ui-selected")) {
-            gs.Image.all[1].wrapper.addClass("ui-selected");
-            gs.Image.all[1].parent.select(gs.Image.all[1]);
+         }
+         //If one semi-open hand, move first image-selection.
+         if(obj.pointables.length >= 3 && obj.pointables.length < 5 && obj.hands.length == 1) {
+            //Draw new cursor
+            LeapEx.draw(targets);
+            //Place first image selected near cursor
+            gs.Image.all[selection].place(LX, LY);
+            if(!gs.Image.all[selection].wrapper.hasClass("ui-selected")) {
+            gs.Image.all[selection].wrapper.addClass("ui-selected");
+            gs.Image.all[selection].parent.select(gs.Image.all[selection]);
+           } else {
+              
            }
           }
-          //If Two hands and index finger pointing with left hand open, move right half of image.
-          else if(obj.pointables.length <= 6 && obj.pointables.length > 4 && obj.hands.length == 2) {
-
-              if(matches == 0) { hovering = true; }
-              gs.Image.all[2].place(LX, LY);
-            
-              locX = Math.abs(LX);
-              locY = Math.abs(LY);
-              offset.left = locX;
-              offset.top = locY;
-              gs.Image.all[2].image.offset = offset;
-
-              if(!gs.Image.all[2].wrapper.hasClass("ui-selected")) {
-              gs.Image.all[2].wrapper.addClass("ui-selected");
-              gs.Image.all[2].parent.select(gs.Image.all[1]);
+          //If two hands and index finger pointing with left hand open, move second image-selection.
+          else if(obj.pointables.length >= 5 && obj.hands.length == 2) {
+              //Draw new cursor
+              LeapEx.draw(targets);
+              //Place second image selected near cursor
+              gs.Image.all[selection2].place(LX, LY);
+              if(!gs.Image.all[selection2].wrapper.hasClass("ui-selected")) {
+              gs.Image.all[selection2].wrapper.addClass("ui-selected");
+              gs.Image.all[selection2].parent.select(gs.Image.all[selection2]);
              }
           }
           //Merge gesture. Two fists.
           else if(obj.pointables.length == 0 && obj.hands.length == 2 && matches == 0) {
-            var matchset = gs.Image.all[2].match(gs.Image.all[1]);
-            var translation = gs.Image.all[2].estimateTranslation(matchset);
-            console.log(translation);
-            var merged = gs.Image.all[1].overlay(gs.Image.all[2], new gs.Transform().translate(translation));
-            console.log(merged);
-            matches = matches + 1;
-            hovering = false;
-            LeapEx.ctx.clearRect(0, 0, LeapEx.width, LeapEx.height);
+
+            //Draw new cursor
+            //LeapEx.draw(targets);
+            //Error prevention for presenting smoothly.
+            switch(selection) {
+              case 1: selection2 = 2; break;
+              case 2: selection = 1; selection2 = 2; break;
+              case 3: selection2 = 4; break;
+              case 4: selection = 3; selection2 = 4; break;
+              case 5: selection2 = 6; break;
+              case 6: selection = 5; selection2 = 6; break; 
+            }
+            //Merging the selected images
+            //if(spinner === undefined) {
+
+              var matchset = gs.Image.all[selection2].match(gs.Image.all[selection]);
+              var translation = gs.Image.all[selection2].estimateTranslation(matchset);
+              console.log(translation);
+              var merged = gs.Image.all[selection].overlay(gs.Image.all[selection2], new gs.Transform().translate(translation));
+              console.log(merged);
+              matches = matches + 1;
+            //}
+            stopSpin();
+            
           }
-          else{
-            if(hovering == false) {
-              gs.Image.all[1].wrapper.removeClass("ui-selected");
-              gs.Image.all[1].parent.deselect(gs.Image.all[1]);
-              gs.Image.all[2].wrapper.removeClass("ui-selected");
-              gs.Image.all[2].parent.deselect(gs.Image.all[2]);
-              LeapEx.ctx.clearRect(0, 0, LeapEx.width, LeapEx.height);
-           }
+          else {
+            //Do nothing
           }
-        
+          gesture = "";
+          previousFrame = frame;
         }
       }
     };
@@ -193,26 +286,34 @@ var LeapEx = {
       do {
           curleft += obj.offsetLeft;
           curtop += obj.offsetTop;
-         } while (obj = obj.offsetParent);
+         } while (obj == obj.offsetParent);
 
       return {X:curleft,Y:curtop};
     }
   },
 
   draw: function(targets) {
+
     LeapEx.ctx.clearRect(0, 0, LeapEx.width, LeapEx.height);
-    LeapEx.ctx.beginPath();
+    
     for (var i=0; i<targets.length; i++) {
+      //LeapEx.ctx.clearRect(0, 0, LeapEx.width, LeapEx.height);
+      LeapEx.ctx.beginPath();
       var target = targets[i];
       LeapEx.ctx.arc(LeapEx.scale(target.x, LeapEx.leapMinX, LeapEx.leapMaxX, -100, LeapEx.width),
                      LeapEx.scale(target.y, LeapEx.leapMinY, LeapEx.leapMaxY, LeapEx.height, -100),
                      10, 0, Math.PI*2, true);
       LeapEx.ctx.closePath();
-      LeapEx.ctx.fillStyle = "white";
+      LeapEx.ctx.globalAlpha = 0.5;
+      LeapEx.ctx.fillStyle = "green";
       LeapEx.ctx.fill();
-      //LeapEx.ctx.lineWidth = 5;
-      //LeapEx.ctx.strokeStyle = '#003300';
-      //LeapEx.ctx.stroke();
+      LeapEx.ctx.lineWidth = 5;
+      LeapEx.ctx.strokeStyle = '#003300';
+      LeapEx.ctx.beginPath();
+      LeapEx.ctx.stroke();
+      LeapEx.ctx.closePath();
+      //LeapEx.ctx.globalCompositeOperation = 'source-over';
+      
     }
   },
 
